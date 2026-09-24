82cd8a916180cc687963cd42f8f4459c29d2c6db
Akshit_Anand <akshitanand008@gmail.com>
2026-09-24 22:27:50 +0530

    indent

--- home.nix
+++ home.nix
@@ -1,63 +1,65 @@
 { config, pkgs, inputs, ... }:
 
 {
-  home.username = "akshit";
-  home.homeDirectory = "/home/akshit";
-  home.stateVersion = "26.05";
-  home.packages = with pkgs; [
-    # Neovim
-    neovim
-    nil clang-tools lua-language-server
-    tree-sitter
+    home.username = "akshit";
+    home.homeDirectory = "/home/akshit";
+    home.stateVersion = "26.05";
+    home.packages = with pkgs; 
+    [
+        # Neovim
+        neovim
+        nil clang-tools lua-language-server
+        tree-sitter
 
-    # Desktop
-    alacritty
-    fuzzel j4-dmenu-desktop
-    noctalia-shell
-    brightnessctl playerctl libnotify
-    wayland-utils wev wl-clipboard xwayland-satellite
+        # Desktop
+        alacritty
+        fuzzel
+        noctalia-shell
+        brightnessctl playerctl libnotify
+        wayland-utils wev wl-clipboard xwayland-satellite
 
-    # Development
-    gcc
-    gdb
-    python3
-    iverilog
-    nasm
+        # Development
+        gcc
+        gdb
+        python3
+        iverilog
+        nasm
 
-    # Utilities
-    (nnn.override { withNerdIcons = true; })
-    bc lsd bat tealdeer
-    fd file ripgrep
-    unzip zip gzip
-    jq curl
-    cmake gnumake cargo
-    stow fzf tmux
-    taskwarrior3 btop
+        # Utilities
+        (nnn.override { withNerdIcons = true; })
+        bc lsd bat tealdeer
+        fd file ripgrep
+        unzip zip gzip
+        jq curl
+        cmake gnumake cargo
+        stow fzf tmux
+        taskwarrior3 btop
 
-    # Aesthetics
-    bibata-cursors
-    nerd-fonts.jetbrains-mono
-    starship
+        # Aesthetics
+        bibata-cursors
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.hack
+        starship
 
-    # Others
-    thunderbird
-    mpv
-    (texliveBasic.withPackages (ps: with ps; [ latexmk ]))
-    libreoffice pdfarranger
-    zathura kdePackages.okular
-  ];
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "zsh-vi-mode";
-        src = pkgs.zsh-vi-mode;
-        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
-      }
+        # Others
+        thunderbird
+        mpv
+        (texliveBasic.withPackages (ps: with ps; [ latexmk ]))
+        libreoffice pdfarranger
+        zathura kdePackages.okular
     ];
-    initContent = "source /home/akshit/shellscripts/init.sh";
-  };
+
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
+        plugins = [
+        {
+            name = "zsh-vi-mode";
+            src = pkgs.zsh-vi-mode;
+            file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+        }
+        ];
+        initContent = "source /home/akshit/shellscripts/init.sh";
+    };
 }
